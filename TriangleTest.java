f8acdd479b678216faf24ae2d6af6e7a22b09929
Japneet Kaur <japneetkaur@Japneets-MacBook-Pro.local>
2021-10-29 21:08:10 -0400

    wrote test cases for triangle

--- TriangleTest.java
+++ TriangleTest.java
@@ -13,22 +13,69 @@ public class TriangleTest {
 
     public TriangleTest () {
         this.t = new Triangle ();
-    }
+    } // end constructor
 
     @Test
     public void testTriangle1 () {
-        assertEquals ("Equilateral", t.isTriangle1 (5, 5, 5));
-    }
+        // Boundary Value Testing for three sides a, b, c with the following range:
+        // side a: [1, 61]
+        // side b: [11, 99]
+        // side c: [15, 35]
+        assertEquals ("Isosceles", t.isTriangle1 (1, 60, 60));
+        assertEquals ("Isosceles", t.isTriangle1 (2, 60, 60));
+        assertEquals ("Equilateral", t.isTriangle1 (60, 60, 60));
+        assertEquals ("Isosceles", t.isTriangle1 (61, 60, 60));
+        assertEquals ("NotATriangle", t.isTriangle1 (31, 25, 60));
+        assertEquals ("NotATriangle", t.isTriangle1 (31, 26, 60));
+        assertEquals ("NotATriangle", t.isTriangle1 (31, 94, 60));
+        assertEquals ("NotATriangle", t.isTriangle1 (31, 95, 60));
+        assertEquals ("Scalene", t.isTriangle1 (31, 60, 30));
+        assertEquals ("Isosceles", t.isTriangle1 (31, 60, 31));
+        assertEquals ("Scalene", t.isTriangle1 (31, 60, 89));
+        assertEquals ("Scalene", t.isTriangle1 (31, 60, 90));
+        assertEquals ("Isosceles", t.isTriangle1 (31, 60, 60));
+    } // end testTriangle1 ()
 
     @Test
     public void testTriangle2 () {
-        assertEquals ("Equilateral", t.isTriangle2 (5, 5, 5));
-    }
+        // Boundary Value Testing for three sides a, b, c with the following range:
+        // side a: [1, 61]
+        // side b: [11, 99]
+        // side c: [15, 35]
+        assertEquals ("Isosceles", t.isTriangle2 (1, 60, 60));
+        assertEquals ("Isosceles", t.isTriangle2 (2, 60, 60));
+        assertEquals ("Equilateral", t.isTriangle2 (60, 60, 60));
+        assertEquals ("Isosceles", t.isTriangle2 (61, 60, 60));
+        assertEquals ("NotATriangle", t.isTriangle2 (31, 25, 60));
+        assertEquals ("NotATriangle", t.isTriangle2 (31, 26, 60));
+        assertEquals ("NotATriangle", t.isTriangle2 (31, 94, 60));
+        assertEquals ("NotATriangle", t.isTriangle2 (31, 95, 60));
+        assertEquals ("Scalene", t.isTriangle2 (31, 60, 30));
+        assertEquals ("Isosceles", t.isTriangle2 (31, 60, 31));
+        assertEquals ("Scalene", t.isTriangle2 (31, 60, 89));
+        assertEquals ("Scalene", t.isTriangle2 (31, 60, 90));
+        assertEquals ("Isosceles", t.isTriangle2 (31, 60, 60));
+    } // end testTriangle2 ()
 
     @Test
     public void testTriangle3 () {
-        
-        assertEquals ("Equilateral", t.isTriangle3 (5, 5, 5));
-    }
+        // Boundary Value Testing for three sides a, b, c with the following range:
+        // side a: [1, 61]
+        // side b: [11, 99]
+        // side c: [15, 35]
+        assertEquals ("Isosceles", t.isTriangle3 (1, 60, 60));
+        assertEquals ("Isosceles", t.isTriangle3 (2, 60, 60));
+        assertEquals ("Equilateral", t.isTriangle3 (60, 60, 60));
+        assertEquals ("Isosceles", t.isTriangle3 (61, 60, 60));
+        assertEquals ("NotATriangle", t.isTriangle3 (31, 25, 60));
+        assertEquals ("NotATriangle", t.isTriangle3 (31, 26, 60));
+        assertEquals ("NotATriangle", t.isTriangle3 (31, 94, 60));
+        assertEquals ("NotATriangle", t.isTriangle3 (31, 95, 60));
+        assertEquals ("Scalene", t.isTriangle3 (31, 60, 30));
+        assertEquals ("Isosceles", t.isTriangle3 (31, 60, 31));
+        assertEquals ("Scalene", t.isTriangle3 (31, 60, 89));
+        assertEquals ("Scalene", t.isTriangle3 (31, 60, 90));
+        assertEquals ("Isosceles", t.isTriangle3 (31, 60, 60));
+    } // end testTriangle3 ()
 
-}
+} // end TriangleTest class definition
